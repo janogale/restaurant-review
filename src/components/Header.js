b84f6ea9c3fe06f83823982f275528a5219e0125
Mukhtar Janagale <janogale@users.noreply.github.com>
2021-06-06 17:46:20 +0300

    Add account dropdown in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,19 @@
-import { Flex, Link, Avatar, Icon, Text, HStack } from "@chakra-ui/react";
 import React from "react";
+import { useRouter } from "next/router";
+import {
+  Flex,
+  Link,
+  Icon,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  Button,
+} from "@chakra-ui/react";
 import { MdRateReview } from "react-icons/md";
+import { BiUserCircle } from "react-icons/bi";
 
 export default function Layout() {
   return (
@@ -19,11 +32,32 @@ export default function Layout() {
         </Link>
       </Flex>
       <Flex align="center">
-        <Text mr={3}>Accounts</Text>
-        <Link href="/">
-          <Avatar size="sm" src="https://bit.ly/broken-link" />
-        </Link>
+        <AccountsModal />
       </Flex>
     </HStack>
+  );
+}
+
+function AccountsModal() {
+  const router = useRouter();
+  return (
+    <Menu>
+      <MenuButton
+        bg="transparent"
+        _expanded={{ bg: "blue.50" }}
+        _focus={{ border: "none", bg: "transparent" }}
+        as={Button}
+        rightIcon={<BiUserCircle />}
+      >
+        Accounts
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Profile</MenuItem>
+        <MenuDivider />
+        <MenuItem onClick={() => router.push("/login")}>Sign in</MenuItem>
+        <MenuItem onClick={() => router.push("/login")}>Sign up</MenuItem>
+        <MenuItem onClick={() => router.push("/login")}>Logout</MenuItem>
+      </MenuList>
+    </Menu>
   );
 }

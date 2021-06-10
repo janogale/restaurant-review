ed89bffc55d985a7506601a120cea41dd070324c
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-06-10 11:08:40 +0300

    Create users on calling signup API

--- src/pages/api/accounts/signin.js
+++ src/pages/api/accounts/signin.js
@@ -4,14 +4,12 @@ import firebase from "@/lib/firebase";
 export default async (req, res) => {
   const { password, email } = req.body;
   try {
-    if (firebase) {
-      const user = await firebase
-        .auth()
-        .signInWithEmailAndPassword(email, password);
+    const user = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
 
-      res.statusCode = 200;
-      res.status(200).json(user);
-    }
+    res.statusCode = 200;
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json(error);
   }
--- src/pages/api/accounts/signup.js
+++ src/pages/api/accounts/signup.js
@@ -1,16 +1,31 @@
 import firebase from "../../../../lib/firebase";
+import admin from "../../../../lib/firebase-admin";
 
+const db = admin.firestore();
+
+// eslint-disable-next-line consistent-return
 export default async (req, res) => {
   const { password, email } = req.body;
-  try {
-    if (firebase) {
-      const user = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password);
 
-      res.statusCode = 200;
-      res.status(200).json(user);
-    }
+  if (!firebase)
+    return res.status(500).json({ message: "Firebase is undefined" });
+  if (!db) return res.status(500).json({ message: "db is undefined" });
+
+  try {
+    const response = await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password);
+
+    const { user } = response;
+
+    // store user to firestore
+    await db.collection("users").doc(user.uid).set({
+      uid: user.uid,
+      email: user.email,
+      role: "regular",
+    });
+
+    res.status(200).json(user);
   } catch (error) {
     res.status(400).json(error);
   }

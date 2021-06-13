9c4ade845a4d200b0d9d0b46c73a6f09b6edfd0c
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-06-13 16:42:06 +0300

    Add document title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head'
 import Container from "./Container";
 import Header from "./Header";
 import Main from "./Main";
@@ -7,6 +8,10 @@ import Footer from "./Footer";
 export default function Layout({ children }) {
   return (
     <Container>
+    <Head>
+        <title>Restuarant Review App</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Main>{children}</Main>
       <Footer />
